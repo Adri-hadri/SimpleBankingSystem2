b4632df5453d0c827024c79204897317274dcf88
Adri-Hadri <adriana.redep.matosevic@gmail.com>
2021-10-18 12:05:21 +0200

    2/4 SBS

---
+++ src/banking2/Account.java
@@ -0,0 +1,59 @@
+package banking2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Account {
+    Scanner scanner = new Scanner(System.in);
+    String cardNumber;
+    String pin;
+    int balance;
+
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public String getPin() {
+        return pin;
+    }
+
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    static Account createNewRandom() {
+        Account a = new Account ();
+        // String accNumNoCheck = "400000" + String.format("%09d", (long) (Math.random() * 999999999L)) + "1";
+        String accNumNoCheck = "400000" + String.format("%09d", (long) (Math.random() * 999999999L));
+        a.setCardNumber(accNumNoCheck);
+        a.setCardNumber(accNumNoCheck + generateLuhn(accNumNoCheck));
+        a.setPin(String.format("%04d", (long) (Math.random() * 9999)));
+
+        return a;
+    }
+
+    public String toString() {
+        return "Account{" +
+                "cardNumber='" + cardNumber + '\'' +
+                ", pin='" + pin + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    private static int generateLuhn (String number){
+        return 0;
+    }
+}
